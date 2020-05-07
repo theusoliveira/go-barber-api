85fcec134e5c6e4602a06fb324f02a45f7d18422
Matheus Oliveira <matheusoliveira@Matheuss-MacBook-Pro.local>
2020-05-07 12:57:11 -0300

    Change folders names

--- appgobarber/src/pages/SignIn/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { Container } from './styles';
-
-const SignIn: React.FC = () => {
-  return <Container />;
-};
-
-export default SignIn;
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
-
-import Route from './Route';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-import Dashboard from '../pages/Dashboard';
 
-const Routes: React.FC = () => (
-  <Switch>
-    <Route exact path="/" component={SignIn} />
-    <Route path="/signup" component={SignUp} />
+const Auth = createStackNavigator();
 
-    <Route path="/dashboard" component={Dashboard} isPrivate />
-  </Switch>
+const AuthRoutes: React.FC = () => (
+  <Auth.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#312e38' },
+    }}
+  >
+    <Auth.Screen name="SignIn" component={SignIn} />
+    <Auth.Screen name="SignUp" component={SignUp} />
+  </Auth.Navigator>
 );
 
-export default Routes;
+export default AuthRoutes;
