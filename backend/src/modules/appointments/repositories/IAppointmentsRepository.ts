4da3e5bdb46bdc37644151edd7a1c7014616f115
Matheus <matheushenriqueoliveira@outlook.com>
2020-06-23 08:29:45 -0300

    List appointments available in day

--- backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,11 +1,15 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointments';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(
     date: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
+  findAllInDayFromProvider(
+    date: IFindAllInDayFromProviderDTO,
   ): Promise<Appointment[]>;
 }
